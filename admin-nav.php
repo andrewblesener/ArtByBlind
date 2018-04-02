8615beddd26a13e775dc549fbdb9aa9ec0314997
andrewblesener <andrewblesener@gmail.com>
2018-04-02 12:11:11 -0500

    Add files via upload
    admin navigation is now php file 'admin-nav', Use admin-template for creating admin pages

---
+++ admin-nav.php
@@ -0,0 +1,36 @@
+<div class="side-menu">
+	<nav class="navbar navbar-default" role="navigation">
+	<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<div class="brand-wrapper">
+
+
+			<!-- Main Menu -->
+				<div class="side-menu-container">
+					<ul class="nav navbar-nav">
+
+					<!-- Dropdown-->
+						<li class="panel panel-default" id="dropdown">
+							<a data-toggle="collapse" href="#dropdown-lvl1">
+							<span class="glyphicon glyphicon-user"></span>PRODUCTS<span class="caret"></span>
+							</a>
+
+						<!-- Dropdown level 1 -->
+							<div id="dropdown-lvl1" class="panel-collapse collapse">
+								<div class="panel-body">
+									<ul class="nav navbar-nav">
+										<li><a href="admin-products.php">VIEW</a></li>
+										<li><a href="admin-add-products.php">ADD</a></li>
+									</ul>
+								</div>
+							</div>
+						</li>
+					<li><a href="#"><span class="glyphicon glyphicon-fire"></span>ORDERS</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-fire"></span>CUSTOMERS</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-cloud"></span>USERS</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</nav>
+</div>
--- admin-products.php
+++ admin-products.php
@@ -6,43 +6,7 @@
 </head>
 <body>
 <?php include('nav.php');?>
-   <div class="side-menu">
-    
-    <nav class="navbar navbar-default" role="navigation">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-        <div class="brand-wrapper">
-
- 
-    <!-- Main Menu -->
-    <div class="side-menu-container">
-        <ul class="nav navbar-nav">
-
-                       <!-- Dropdown-->
-            <li class="panel panel-default" id="dropdown">
-                <a data-toggle="collapse" href="#dropdown-lvl1">
-                    <span class="glyphicon glyphicon-user"></span>PRODUCTS<span class="caret"></span>
-                </a>
-
-                <!-- Dropdown level 1 -->
-                <div id="dropdown-lvl1" class="panel-collapse collapse">
-                    <div class="panel-body">
-                        <ul class="nav navbar-nav">
-                           <li><a href="admin/index.php">VIEW</a></li>
-							<li><a href="admin/add.php">ADD</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </li>
-            <li><a href="#"><span class="glyphicon glyphicon-fire"></span>ORDERS</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-fire"></span>CUSTOMERS</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-cloud"></span>USERS</a></li>
-        </ul>
-    </div>
-		</div>
-		</div>
-</nav>
-    </div>
+<?php include('admin-nav.php')?>
  
     <div class="col-lg-offset-3 col-lg-8" style="margin-top:5em;">
  

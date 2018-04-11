60238844f17840fd82e6ff57de81b69d9b482409
isast <36216746+isast@users.noreply.github.com>
2018-04-10 20:11:47 -0500

    login update

--- authenticate.php
+++ authenticate.php
@@ -1,36 +1,42 @@
-
 <?php
-require 'database-config.php'; //Database Connection
-session_start(); //Start the session
+require 'database-config.php'; //Db Conn
+session_start();
 
 if(isset($_POST['username']))
-{ $username = $_POST['username'];}
+{ 
+	$username = $_POST['username'];
+}
 
 if(isset($_POST['password']))
-{ $password = $_POST['password'];}
+{ 
+	$password = $_POST['password']; 
+}
 
-//Check whether the entered username/password pair exist in the Database
+// check whether the entered username/password pair exist in the db
 $q = 'SELECT * FROM users WHERE username=:username AND password=:password';
 $query = $dbh->prepare($q);
 $query->execute(array(':username' => $username, ':password' => $password));
 
 if($query->rowCount() == 0)
-{ header('Location: index.php?err=1');}
-else
-{//fetch the result as associative array
+{ header('Location: index.php?err=1'); }
+
+
+else{
+	//fetch the result as associative array
 	$row = $query->fetch(PDO::FETCH_ASSOC);
-
-
-//store the fetched details into $_SESSION
-$_SESSION['sess_user_id'] = $row['id'];
-$_SESSION['sess_username'] = $row['username'];
-$_SESSION['sess_userrole'] = $row['role'];
-
-if($_SESSION['sess_userrole'] == "suadmin")
-{ header('Location: adminHome.php');}
-elseif($_SESSION['sess_userrole'] == "admin")
-{ header('Location: adminHome.php');}
-else{header('Location: index.php');
+	
+	//Store the fetched details into $_SESSION
+	$_SESSION['sess_user_id'] = $row['id'];
+	$_SESSION['sess_username'] = $row['username'];
+	$_SESSION['sess_userrole'] = $row['role'];
+	
+	if( $_SESSION['sess_userrole'] == "suadmin")
+	{ header('Location: suadminhome.php');
 	}
-}
-?>
+	elseif( $_SESSION['sess_userrole'] == "Admin")
+	{ header('Location: adminhome.php');
+	}
+	else
+	{ header('Location: userhome.php');
+	}
+}?>
